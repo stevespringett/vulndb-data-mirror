7394f1a0a50d4cc87706beb03c609836ce961dbb
Steve Springett <steve@springett.us>
2019-09-09 14:26:10 -0500

    Added release script

---
+++ release.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
+export PATH=JAVA_HOME/bin:$PATH
+
+read -p "Really deploy to Maven Central repository (Y/N)? "
+if ( [ "$REPLY" == "Y" ] ) then
+
+  mvn clean
+  mvn release:clean release:prepare release:perform -Prelease -X -e | tee release.log
+  mvn github-release:release
+
+else
+  echo -e "Exit without deploy"
+fi
